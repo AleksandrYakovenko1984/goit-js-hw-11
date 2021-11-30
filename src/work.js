d1aa80c26c3015e92c4d6cd96578aee71e2d7744
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2021-12-01 00:48:39 +0200

    добавил стили

--- src/work.js
+++ src/work.js
@@ -5,12 +5,15 @@ const galeryList = document.querySelector('.gallery')
 const button = document.getElementsByTagName("button")
 const btnLoadMore = document.querySelector('.load-more')
 const modalWindow = document.querySelector('.modal')
+const closeBtn = document.querySelector('.close')
 import cardList from "./cardList.hbs"
+import Notiflix from "notiflix"
 
+import img from './bigPicture.hbs'
 formSearch.addEventListener('submit', onSearch);
 btnLoadMore.addEventListener('click', onLoadMore)
-galeryList.addEventListener('click', openModal)
-
+ galeryList.addEventListener('click', openModal)
+closeBtn.addEventListener('click', closeModal)
  
 
 let page = 1
@@ -22,7 +25,9 @@ let searchQuery = "";
 
 
 function createCardList(data) {
+   
   return data.map(cardList).join('')
+
 }
 
 function onSearch(e) {
@@ -32,7 +37,7 @@ function onSearch(e) {
   
   page = 1;
   fetchPicture(searchQuery)
-
+ 
 
 
 }
@@ -41,6 +46,7 @@ function onSearch(e) {
 
 async function fetchPicture(searchQuery) {
   btnLoadMore.classList.add('is-hidden')
+ 
    if (searchQuery) {
 
     const BASE_URL = `https://pixabay.com/api/?key=24572731-dd81b25f4ab1454a48aca0751`
@@ -54,10 +60,12 @@ async function fetchPicture(searchQuery) {
        });
      
      if (per_page * page < data.totalHits) {
-          btnLoadMore.classList.remove('is-hidden')
-     }
+       btnLoadMore.classList.remove('is-hidden')
+       
+     } 
      const renderCardList = createCardList(data.hits)
-galeryList.innerHTML = renderCardList
+     galeryList.innerHTML = renderCardList
+    
     //  createCardList(data.hits)
     }
 }
@@ -67,6 +75,15 @@ function onLoadMore() {
   
 }
 
-function openModal(condition) {
+function openModal(data) {
+  
   modalWindow.classList.add('is-open')
+//   console.log(img);
+//  modalWindow.innerHTML = img
+}
+function closeModal() {
+  modalWindow.classList.remove('is-open')
+}
+function showError() {
+     return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results");
 }
