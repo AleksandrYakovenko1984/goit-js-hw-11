a5d392b04941ae2ae700fbdb3ecf77b471fefee0
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2021-11-30 18:08:47 +0200

    .

--- src/work.js
+++ src/work.js
@@ -1,32 +1,72 @@
 
 const formSearch = document.getElementById('search-form')
 const body = document.querySelector('.body')
+const galeryList = document.querySelector('.gallery')
 const button = document.getElementsByTagName("button")
+const btnLoadMore = document.querySelector('.load-more')
+const modalWindow = document.querySelector('.modal')
+import cardList from "./cardList.hbs"
 
 formSearch.addEventListener('submit', onSearch);
+btnLoadMore.addEventListener('click', onLoadMore)
+galeryList.addEventListener('click', openModal)
+
+ 
+
+let page = 1
+let per_page = 40
+
+let searchQuery = "";
+
+
+
+
+function createCardList(data) {
+  return data.map(cardList).join('')
+}
 
 function onSearch(e) {
     e.preventDefault()
     const form = e.currentTarget
-    const searchQuery = form.elements.searchQuery.value
-    console.log(searchQuery);
-    fetchPicture(searchQuery)
-        // .then().catch(error => console.log(error))
+     searchQuery = form.elements.searchQuery.value
+  
+  page = 1;
+  fetchPicture(searchQuery)
+
 
 
 }
 
 
 
- function fetchPicture(searchQuery) {
+async function fetchPicture(searchQuery) {
+  btnLoadMore.classList.add('is-hidden')
    if (searchQuery) {
-      const url = `https://pixabay.com/api/?key=24572731-dd81b25f4ab1454a48aca0751&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true?fields=webformatURL,largeImageURL,tags,likes,views,comments,downloads`
-      return fetch(url)
+
+    const BASE_URL = `https://pixabay.com/api/?key=24572731-dd81b25f4ab1454a48aca0751`
+      const url = `${BASE_URL}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page*page}`
+      const data = await fetch(url)
         .then(response => {
          if (!response.ok) {
            throw Error(response.statusText)
-       }
-   return   response.json()
-    })   
+         }
+         return response.json()
+       });
+     
+     if (per_page * page < data.totalHits) {
+          btnLoadMore.classList.remove('is-hidden')
+     }
+     const renderCardList = createCardList(data.hits)
+galeryList.innerHTML = renderCardList
+    //  createCardList(data.hits)
+    }
 }
+function onLoadMore() {
+  page = page + 1
+  fetchPicture(searchQuery)
+  
+}
+
+function openModal(condition) {
+  modalWindow.classList('is-open')
 }
