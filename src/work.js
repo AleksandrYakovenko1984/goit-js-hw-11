9a6f23e5ba4cd5763bfad7cff8eabf0db81eaf34
AleksandrYakovenko1984 <jakovenko.gav@gmail.com>
2021-12-01 21:07:32 +0200

    modal

--- src/work.js
+++ src/work.js
@@ -1,4 +1,4 @@
-
+ const BASE_URL = `https://pixabay.com/api/?key=24572731-dd81b25f4ab1454a48aca0751`
 const formSearch = document.getElementById('search-form')
 const body = document.querySelector('.body')
 const galeryList = document.querySelector('.gallery')
@@ -6,13 +6,20 @@ const button = document.getElementsByTagName("button")
 const btnLoadMore = document.querySelector('.load-more')
 const modalWindow = document.querySelector('.modal')
 const closeBtn = document.querySelector('.close')
-import cardList from "./cardList.hbs"
 import Notiflix from "notiflix"
-
+import cardList from "./cardList.hbs"
 import img from './bigPicture.hbs'
 formSearch.addEventListener('submit', onSearch);
-btnLoadMore.addEventListener('click', onLoadMore)
- galeryList.addEventListener('click', openModal)
+btnLoadMore.addEventListener('click',  onLoadMore)
+galeryList.addEventListener('click', (elem) => {
+  const condition = elem.target.nodeName === "IMG"
+ 
+  
+  if (condition) {
+    openModal()
+  }
+  
+ } )
 closeBtn.addEventListener('click', closeModal)
  
 
@@ -27,16 +34,20 @@ let searchQuery = "";
 function createCardList(data) {
    
   return data.map(cardList).join('')
-
+ 
 }
+function createCardModal(data) {
 
+  return data.map(img)
+ 
+}
 function onSearch(e) {
     e.preventDefault()
     const form = e.currentTarget
      searchQuery = form.elements.searchQuery.value
   
   page = 1;
-  fetchPicture(searchQuery)
+  fetchPicture(searchQuery).then().catch((error)=>{showError()})
  
 
 
@@ -49,9 +60,10 @@ async function fetchPicture(searchQuery) {
  
    if (searchQuery) {
 
-    const BASE_URL = `https://pixabay.com/api/?key=24572731-dd81b25f4ab1454a48aca0751`
+   
       const url = `${BASE_URL}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page*page}`
-      const data = await fetch(url)
+      
+     const data = await fetch(url)
         .then(response => {
          if (!response.ok) {
            throw Error(response.statusText)
@@ -62,28 +74,37 @@ async function fetchPicture(searchQuery) {
      if (per_page * page < data.totalHits) {
        btnLoadMore.classList.remove('is-hidden')
        
-     } 
+     }
+     const renderModal = createCardModal(data.hits)
      const renderCardList = createCardList(data.hits)
      galeryList.innerHTML = renderCardList
-    
+     
+     renderModal.forEach(element => {
+       
+       if (openModal) {
+         modalWindow.innerHTML = element
+         modalWindow.append(closeBtn);
+       }
+     })
     //  createCardList(data.hits)
     }
 }
 function onLoadMore() {
   page = page + 1
   fetchPicture(searchQuery)
-  
+ 
 }
 
-function openModal(data) {
+function openModal() {
   
   modalWindow.classList.add('is-open')
-//   console.log(img);
-//  modalWindow.innerHTML = img
+
+//  modalWindow.insertAdjacentHTML('beforeend', img)
 }
 function closeModal() {
   modalWindow.classList.remove('is-open')
+  
 }
 function showError() {
-     return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results");
+     return Notiflix.Notify.failure("");
 }
